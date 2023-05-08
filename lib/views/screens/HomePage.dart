0a434a981bbd482b5210bbbe1a46f0d536dcc0c9
darshankumar09 <darshansutariya2003@gmail.com>
2023-05-09 00:34:42 +0530

    search_engine_update

--- lib/views/screens/HomePage.dart
+++ lib/views/screens/HomePage.dart
@@ -328,7 +328,7 @@ class _HomePageState extends State<HomePage> {
                               ),
                             ),
                             const SizedBox(
-                              height: 8,
+                              height: 2,
                             ),
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
